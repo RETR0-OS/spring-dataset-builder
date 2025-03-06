eff53f3db7373affe953533552252c6b365882aa
RETR0-OS <aadityajindal12@gmail.com>
2025-03-06 02:07:02 -0700

    initial fix for CORS error

--- datasetBuilder/datasetBuilder/src/main/java/com/aaditya/honors/datasetBuilder/Controllers/HomeController.java
+++ datasetBuilder/datasetBuilder/src/main/java/com/aaditya/honors/datasetBuilder/Controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.aaditya.honors.datasetBuilder.Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class HomeController {
 

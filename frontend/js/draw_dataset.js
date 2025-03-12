458bdf8850007fd9003718465cee956a15db758b
RETR0-OS <aadityajindal12@gmail.com>
2025-03-12 16:20:50 -0700

    redirect to dashboard on creation

--- frontend/js/draw_dataset.js
+++ frontend/js/draw_dataset.js
@@ -231,7 +231,7 @@ function submitDataset(){
             "Content-Type": "application/json",
         },
         body: JSON.stringify(request),
-    }).then((response) => response.json()).then((json) => console.log(json));
+    }).then((response) => response.json()).then((json) => window.location.href = "/frontend/datasets/dashboard/dashboard.html");
     // console.log(request);
 
 }
